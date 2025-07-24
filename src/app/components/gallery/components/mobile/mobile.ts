6fee186c295927ac26d86259870cb2fda6f0e031
ENG_Mostafa <mostafa.fathy22422@gmail.com>
2025-07-25 00:54:32 +0300

    second

--- src/app/components/gallery/components/mobile/mobile.ts
+++ src/app/components/gallery/components/mobile/mobile.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import {NgOptimizedImage} from '@angular/common';
 
 @Component({
   selector: 'app-mobile',
-  imports: [],
+  imports: [
+    NgOptimizedImage
+  ],
   templateUrl: './mobile.html',
   standalone: true,
   styleUrl: './mobile.css'
